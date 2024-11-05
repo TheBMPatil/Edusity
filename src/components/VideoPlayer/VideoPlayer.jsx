4e990eb74c317c4f2f364cb355e3b83a16505619
Bhagvat Mutthe <bhagvatmutthe6@gmail.com>
2024-11-06 01:43:50 +0530

    Trying to resolve Deploy Error v2

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,7 +1,8 @@
 // import React, { useRef } from 'react'
 import './VideoPlayer.css'
 
-import video_1 from '../../assets/video-1.mp4'
+// import video_1 from '../../assets/video-1.mp4'
+import video from '../../assets/video.mp4'
 import { useRef } from 'react';
 const VideoPlayer = ({ playState, setPlayState }) => {
 
@@ -13,7 +14,8 @@ const VideoPlayer = ({ playState, setPlayState }) => {
     }
     return (
         <div className={`video-player ${playState ? '' : 'hide'}`} ref={player} onClick={closePlayer}>
-            <video src={video_1} autoPlay muted controls></video>
+            {/* <iframe width={700} height={700} src="https://www.youtube-nocookie.com/embed/jvwHIRscAHg?si=NqXk0QLhxdd0nTd1&amp;controls=0" title="YouTube video player" allow=" autoplay=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> */}
+            <video src={video} autoPlay muted controls></video>
         </div>
     )
 }
